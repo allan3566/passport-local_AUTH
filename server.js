6c206f322d55e5314542623ebff1f538c14a510c
Allan.Tien <allan3566@gmail.com>
2024-01-03 16:33:07 +0800

    20240103

--- server.js
+++ server.js
@@ -18,15 +18,28 @@ app.get('/', (req,res)=>{
     res.render('index.ejs', {name:'allan'})
 })
 
-app.post('/login', (req,res)=>{
-    res.render('login.ejs')
-})
-
 app.get('/login', (req,res)=>{
     res.render('login.ejs')
 })
 
-app.get('/registerform', (req,res)=>{
+users = [];
+app.post('/register', async (req, res) => {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+      })
+      res.redirect('/login')
+    } catch {
+      res.redirect('/register')
+    }
+    console.log(users);
+  })
+
+app.get('/register', (req,res)=>{
     res.render('register.ejs')
 })
 app.listen(3000)
